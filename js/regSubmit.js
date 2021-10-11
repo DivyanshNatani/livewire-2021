8d45fa3fcf0d89ab8ea5328b81ac7e8448c5bbb4
Palash Mittal <pmittal00@gmail.com>
2021-10-12 00:27:19 +0530

    Updated register

--- js/regSubmit.js
+++ js/regSubmit.js
@@ -10,6 +10,7 @@ function submitRegForm() {
   let mem2_name = document.getElementById("mem2_name").value;
   let mem2_contact = document.getElementById("mem2_contact").value;
   let links = document.getElementById("links").value;
+  let inv = false;
 
   function setInv(id) {
     document.getElementById(id).style.borderColor = "red";
@@ -23,18 +24,21 @@ function submitRegForm() {
 
   if (city == "") {
     setInv("city");
+    inv = true;
   }
   else {
     setNormal("city");
   }
   if (band_name == "") {
     setInv("band_name");
+    inv = true;
   }
   else {
     setNormal("band_name");
   }
   if (genre == "") {
     setInv("genre");
+    inv = true;
   }
   else {
     setNormal("genre");
@@ -53,6 +57,7 @@ function submitRegForm() {
       )
     ) {
       setInv("email");
+      inv = true;
     }
     else {
       setNormal("email");
@@ -60,45 +65,57 @@ function submitRegForm() {
   }
   if (past_ach == "") {
     setInv("past_ach");
+    inv = true;
   }
   else {
     setNormal("past_ach");
   }
   if (num_mem == "") {
     setInv("num_mem");
+    inv = true;
   }
   else {
     setNormal("num_mem");
   }
   if (mem1_name == "") {
     setInv("mem1_name");
+    inv = true;
   }
   else {
     setNormal("mem1_name");
   }
   if (mem1_contact == "" || mem1_contact.length != 10) {
     setInv("mem1_contact");
+    inv = true;
   }
   else {
     setNormal("mem1_contact");
   }
   if (mem2_name == "") {
     setInv("mem2_name");
+    inv = true;
   }
   else {
     setNormal("mem2_name");
   }
   if (mem2_contact == "" || mem2_contact.length != 10) {
     setInv("mem2_contact");
+    inv = true;
   }
   else {
     setNormal("mem2_contact");
   }
   if (links == "") {
     setInv("links");
+    inv = true;
   }
   else {
     setNormal("links");
+  }
+
+  if (inv) {
+    alert("Please fill out all required fields correctly");
+    return;
   }
 
   let formData = {
