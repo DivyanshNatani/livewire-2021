73af75432c328fdb3bd841d2891b633accd38b0b
Palash Mittal <pmittal00@gmail.com>
2021-10-16 09:26:05 +0530

    Removed logs

--- js/main.js
+++ js/main.js
@@ -313,9 +313,9 @@
     let prizeImgs = document.getElementsByClassName('card-image');
     for (var i = 0; i < prizeImgs.length; i++) {
       let element = prizeImgs[i];
-      console.log(element);
+      // console.log(element);
       let h = element.clientWidth;
-      console.log(h);
+      // console.log(h);
       element.parentElement.style.height = h * 5 / 4 + "px";
     }
   });
